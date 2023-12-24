e39687aeab143b03383ce66114fa509067e8c978
alcadeiasani19 <anish2825@gmail.com>
2023-12-24 19:30:12 +0600

    First Commit

--- Modules/Products/app/Http/Controllers/ProductController.php
+++ Modules/Products/app/Http/Controllers/ProductController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-class ProductsController extends Controller
+class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
